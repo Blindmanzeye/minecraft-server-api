4e6ddbbb9715ee675de0fef20a36d943458808b8
Blindmanzeye <shafatiscool@gmail.com>
2026-09-14 15:50:38 -0400

    added chunky commands

--- api/main.py
+++ api/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from mcrcon import MCRcon
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+RCON_HOST = os.getenv("RCON_HOST")
+RCON_PASSWORD = os.getenv("RCON_PASSWORD")
+RCON_PORT = int(os.getenv("RCON_PORT", 25575))
+
+@app.get("/stop-chunky")
+def stop_chunky():
+    with MCRcon(RCON_HOST, RCON_PASSWORD, port=RCON_PORT) as mcr:
+        response = mcr.command("/chunky pause")
+    return {"message": "chunky stopped", "response": response}
+
+@app.get("/start-chunky")
+def start_chunky():
+    with MCRcon(RCON_HOST, RCON_PASSWORD, port=RCON_PORT) as mcr:
+        response = mcr.command("/chunky resume")
+    return {"message": "chunky started", "response": response}
+
+@app.get("/restart-server")
+def restart_server():
+    with MCRcon(RCON_HOST, RCON_PASSWORD, port=RCON_PORT) as mcr:
+        response = mcr.command("/stop")
+    return {"message": "Server restarted", "response": response}
